173a50a1b5859ee81c49d392d909c0c92febe44b
Stephen von Takach <steve@advancedcontrol.com.au>
2014-12-09 17:36:34 +1100

    only manage the list of file for offline caching
    All other functionality is void

--- storage.js
+++ storage.js
@@ -9,179 +9,32 @@
         module = angular.module('coUtils', []);
     }
 
-    // stores
-    var FILE_STORE   = 'files';
-    var JSON_STORE   = 'json';
-    var META_STORE   = 'meta';
-    var STORES       = [FILE_STORE, JSON_STORE, META_STORE];
-
-    // special keys
-    var BLOB         = 'blob';
     var CACHED_FILES = 'files';
-
-    // events
     var DL_EVENT     = 'co-storage-downloading';
-    var FIVE_MINUTES = 5 * 60 * 1000;
 
     module
-        // each store is represented by a different physical db
-        .factory('CacheStore', [
-            '$q',
-            function($q) {
-                return function(name) {
-                    // --------------------------
-                    // initialisation
-                    // --------------------------
-                    var db = new PouchDB(name);
-                    var store = this;
-                    this.db = db;
-
-                    this.compact = function() {
-                        db.compact().catch(function(err) {
-                            console.log('compaction failed for cache db ' + name, err);
-                        });
-                    }
-
-                    // pouchdb stores all revisions of a record until compaction
-                    setInterval(function() {
-                        store.compact();
-                    }, FIVE_MINUTES);
-
-                    // Value handling: values can be objects and simple values -
-                    // cache.meta.put('name', 'benny') is valid, as is
-                    // cache.files.put('file_name', {url: 'http://...'})
-                    // To satisfy pouchdb's record format, the actual value stored
-                    // in the db is an object with this structure:
-                    // {_id: key, _rev: revision, val: your_value}
-                    // When you call get, the value stored in val is returned as is.
-                    // If the record has an associated file, however, the returned
-                    // value is an object with this structure:
-                    // {blob: Blob(), val: your_value}
-                    // This means simple use (e.g with the meta and json stores)
-                    // avoids having to traverse an object to use .val; but does
-                    // mean you need to know ahead of time whether a record will
-                    // be storing a file.
-
-                    // --------------------------
-                    // methods
-                    // --------------------------
-                    this.get = function(key) {
-                        return $q(function(resolve, reject) {
-                            db.get(key, {attachments: true}, function(err, doc) {
-                                if (err) {
-                                    if (err.status == 404)
-                                        resolve(undefined);
-                                    else
-                                        reject(err);
-                                } else {
-                                    if (!doc._attachments)
-                                        return resolve(doc.val);
-                                    db.getAttachment(key, BLOB).then(function(blob) {
-                                        resolve({
-                                            val: doc.val,
-                                            blob: blob
-                                        });
-                                    }).catch(function(err) {
-                                        reject(err);
-                                    });
-                                }
-                            });
-                        });
-                    }
-
-                    this.put = function(key, value, blob) {
-                        var doc = {
-                            _id: key,
-                            val: value
-                        };
-
-                        return $q(function(resolve, reject) {
-                            function put() {
-                                db.put(doc).then(function(result) {
-                                    if (!blob) {
-                                        resolve(true);
-                                    } else {
-                                        return db.putAttachment(key, BLOB, result.rev, blob, blob.type).then(function() {
-                                            resolve(true);
-                                        });
-                                    }
-                                }).catch(function(err) {
-                                    reject(err);
-                                });
-                            }
-
-                            db.get(key).then(function(existing) {
-                                doc._rev = existing._rev;
-                                put();
-                            }).catch(function(err) {
-                                if (err.status == 404)
-                                    put();
-                                else
-                                    reject(err);
-                            })
-                        });
-                    }
-
-                    this.remove = function(key) {
-                        return $q(function(resolve, reject) {
-                            db.get(key, function(err, doc) {
-                                if (err) {
-                                    if (err.status == 404)
-                                        resolve(true);
-                                    else
-                                        reject(err);
-                                } else {
-                                    db.remove(doc).then(function() {
-                                        resolve(true);
-                                    }).catch(function(err) {
-                                        reject(err);
-                                    })
-                                }
-                            });
-                        });
-                    }
-
-                    this.exists = function(key) {
-                        return $q(function(resolve, reject) {
-                            db.get(key, function(err, doc) {
-                                if (err) {
-                                    if (err.status == 404)
-                                        resolve(false);
-                                    else
-                                        reject(err);
-                                } else {
-                                    resolve(true);
-                                }
-                            });
-                        });
-                    }
-                }
-            }
-        ])
-
         .service('cache', [
-            'CacheStore',
+            '$localForage',
             '$rootScope',
             '$http',
             '$q',
-            function(CacheStore, $rootScope, $http, $q) {
-                // --------------------------
-                // initialisation
-                // --------------------------
-                var cache = this;
-
-                STORES.forEach(function(name) {
-                    cache[name] = new CacheStore(name);
-                    cache[name].compact();
-                });
+            function(cache, $rootScope, $http, $q) {
 
                 // --------------------------
                 // Files API
                 // --------------------------
-                var files = this.files;
+                var files = this,
+                    currentJob = $q.reject(false), // Create a resolved promise to start from
+                    downloadingComplete = $q.defer(),
+                    cancelNext = false,
+                    failures = [];
+                
+
+                downloadingComplete.resolve(true);
                 files.downloading = false;
 
-                function _cacheFile(fileList, index, complete, error) {
+
+                function _cacheFile(defer, fileList, index) {
                     // download files sequentially. because ajax requests are async
                     // looping through each url and calling $http.get for each would
                     // result in multiple concurrent requests. instead, the success/
@@ -189,109 +42,144 @@
                     // index until the end of the array is reached. because the
                     // callbacks are called independently of the originating call,
                     // long url lists won't result in long stacks.
-                    if (index >= fileList.length) {
+                    if (index >= fileList.length || cancelNext) {
                         files.downloading = false;
                         $rootScope.$broadcast(DL_EVENT, false);
-                        files.compact();
+                        if (cancelNext) {
+                            downloadingComplete.reject('new list provided');
+                        } else {
+                            downloadingComplete.resolve(fileList);
+                        }
                         return;
                     }
 
                     var file = fileList[index];
-                    var url = file.url;
                     function next() {
-                        _cacheFile(fileList, index + 1, complete, error);
+                        _cacheFile(defer, fileList, index + 1);
                     }
 
-                    files.exists(url).then(function(exists) {
-                        if (exists)
+                    cache.getItem(file).then(function(exists) {
+                        if (exists || cancelNext)
                             return next();
 
-                        $http.get(url, {
+                        // Download the file
+                        $http.get(file, {
                             responseType: 'blob',
                             cache: true
                         }).success(function(blob, status) {
-                            files.put(url, file, blob).then(function() {
-                                complete(url);
+
+                            // Save the file to database
+                            cache.setItem(file, blob).then(function() {
+                                defer.notify(file);
                             }).catch(function(err) {
-                                console.error('Error storing file', url, err);
-                                error(url, 'db', err);
+                                console.error('Error storing file', file, err);
+                                failures.push(file);
                             });
 
                         }).error(function(resp, status) {
-                            console.error('Error requesting file', url, status);
-                            error(url, 'http', status);
+
+                            console.error('Error requesting file', file, status);
+                            failures.push(file);
 
                         }).finally(function() {
+
+                            // Download the next file
                             next();
+
                         });
 
                     // always move to the next file, even on db errors
                     }).catch(function(err) {
-                        console.error('Error calling _exists', url, err);
+                        console.error('Error calling _exists', file, err);
+                        failures.push(file);
                         next();
                     });
                 }
 
-                files.load = function(fileList, complete, error) {
-                    // Optional callback functions
-                    complete = complete || angular.noop;
-                    error = error || angular.noop;
-                    
-                    cache.meta.get(CACHED_FILES).then(function(old_files) {
-                        if (old_files != undefined) {
+                files.load = function(fileList) {
+                    var dc = downloadingComplete,
+                        newDc = $q.defer();
+
+                    downloadingComplete = newDc;
+
+                    currentJob.finally(function () {
+                        currentJob = cache.getItem(CACHED_FILES).then(function (old_files) {
+                            old_files = old_files || [];
+
+                            // Generate a file lookup
                             var new_urls = {};
                             fileList.forEach(function(file) {
-                                new_urls[file.url] = true;
+                                new_urls[file] = true;
                             });
 
+                            // Remove files that are not in the new list
                             old_files.forEach(function(file) {
-                                if (file.url in new_urls)
+                                if (file in new_urls)
                                     return;
-                                files.remove(file.url).then(function() {
-                                    console.log('Removed stale file', file.url);
+
+                                cache.removeItem(file).then(function() {
+                                    console.log('Removed stale file', file);
                                 }).catch(function(err) {
-                                    console.error('Error removing stale file', file.url, err);
+                                    console.error('Error removing stale file', file, err);
                                 });
                             });
-                        }
+                            
+                            // Save the new list
+                            return cache.setItem(CACHED_FILES, fileList).catch(function(err) {
+                                console.error('Error setting cached file list', err);
+                            });
+                        }).finally(function () {
+                            // If there is an existing download occurring we want to stop it first
+                            if (files.downloading) {
+                                cancelNext = true;
+                            }
 
-                        cache.meta.put(CACHED_FILES, fileList).catch(function(err) {
-                            console.error('Error setting cached file list', err);
+                            // Wait for the downloading to stop and start the next download
+                            dc.promise.finally(function () {
+                                if (newDc === downloadingComplete) {
+                                    // Lets start the caching!
+                                    files.downloading = true;
+                                    $rootScope.$broadcast(DL_EVENT, true);
+                                    cancelNext = false;
+                                    failures = [];
+                                    _cacheFile(newDc, fileList, 0);
+                                } else {
+                                    // There is already a new list replacing this one
+                                    newDc.reject(false);
+                                }
+                            });
                         });
-
-                    }).catch(function(err) {
-                        console.error('Unable to load cached file list');
-
-                    }).finally(function() {
-                        files.downloading = true;
-                        $rootScope.$broadcast(DL_EVENT, true);
-                        _cacheFile(fileList, 0, complete, error);
                     });
-                }
+
+                    return downloadingComplete.promise;
+                };
 
                 files.getImage = function(url, img) {
-                    return files.get(url).then(function(doc) {
-                        if (doc === undefined || doc.blob === undefined) {
-                            img.attr('src', null);
+                    return cache.getItem(url).then(function(blob) {
+                        if (blob) {
+                            var src = URL.createObjectURL(blob);
+                            img.attr('src', src);
+                            return function () {
+                                URL.revokeObjectURL(src);
+                            };
                         } else {
-                            img.attr('src', URL.createObjectURL(doc.blob));
-                            img.on('load', function() {
-                                URL.revokeObjectURL(this.src);
-                            });
+                            console.log('File not in cache', url);
+                            return $q.reject('File not downloaded');
                         }
                     });
                 }
 
                 files.getVideo = function(url, video) {
-                    return files.get(url).then(function(doc) {
-                        if (doc === undefined || doc.blob === undefined) {
-                            video.attr('src', null);
+                    return cache.getItem(url).then(function(blob) {
+                        if (blob) {
+                            var src = URL.createObjectURL(blob);
+                            video.attr('src', src);
+                            return function () {
+                                URL.revokeObjectURL(src);
+                            };
                         } else {
-                            video.attr('src', URL.createObjectURL(doc.blob));
-                            video.on('progress', function() {
-                                if (video.prop('buffered').end(0) == video.prop('duration'))
-                                    URL.revokeObjectURL(this.src);
-                            });
+                            console.log('File not in cache', url);
+                            return $q.reject('File not downloaded');
                         }
                     });
                 }
